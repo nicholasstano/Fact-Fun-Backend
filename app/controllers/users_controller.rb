6d74002fe050e3895ae7d7c89f22399a7f0a7be1
nicholasstano <nicholasstano@gmail.com>
2019-08-20 17:06:16 -0400

    first draft

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
     def index 
-        users = User.all
+        users = User.all.order(:id)
         render :json => users
     end
 
     def create 
         user = User.create(name: params[:name], total_points: params[:total_points])
+        render :json => user
+    end 
+
+    def update 
+        user = User.find(params[:id])
+        user.update(total_points: params[:total_points])
         render :json => user
     end 
 end
